58702a8b8ba1565a0a0382588d780faa8bc66ab0
hitthemoney <50669620+hitthemoney@users.noreply.github.com>
2020-06-22 20:13:15 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -127,3 +127,11 @@ resetCanvas = () => {
         document.getElementById("downloadA").setAttribute("download", "Untitled")
     }
 }
+
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = 'It looks like you have been editing something. '
+                            + 'If you leave before saving, your changes will be lost.';
+
+    (e || window.event).returnValue = confirmationMessage;
+    return confirmationMessage;
+});
